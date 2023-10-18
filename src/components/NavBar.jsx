538f372389710badb6977ebffff355ffc9d623ad
Panpaphop <panpaphop9@gmail.com>
2023-10-19 00:40:26 +0700

    feat:add Tel. on footer

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,22 +4,22 @@ import { useNavigate } from "react-router-dom";
 function Navbar() {
   const navigate = useNavigate();
   return (
-    <div
-      className="bg-[#FF2950] text-white text-[40px] font-bold pl-[100px] h-[148px] flex items-center"
-    >
+    <div className="bg-[#FF2950] text-white text-[40px] font-bold pl-[100px] h-[148px] flex items-center">
       <button
-        className="text-white text-[40px] font-bold pl-[100px] h-[148px]"
+        className="text-white text-[40px] font-bold pl-[100px] h-[148px] hover:text-red-900 hover:text-[45px]"
         onClick={() => navigate("/")}
       >
         My Profile
       </button>
       <button
-        className="ml-auto text-[30px] mr-[100px] font-bold"
+        className="ml-auto text-[30px] mr-[100px] font-bold hover:text-red-900"
         onClick={() => navigate("/about-me")}
       >
         About Me
       </button>
-      <button className="text-[30px] mr-[100px] font-bold">Projects</button>
+      <button className="text-[30px] mr-[100px] font-bold hover:text-red-900">
+        Projects
+      </button>
     </div>
   );
 }
